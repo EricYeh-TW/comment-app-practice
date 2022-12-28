c17186aeda460b26c61ee765d1a05ca7688c4b67
Eric Yeh <spu8673@gmail.com>
2022-12-28 17:53:55 +0800

    feat: install styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px sold #000;
+  color: white;
+  background-color: lightblue;
+  text-align: center;
+
+  .item {
+    width: 50px;
+    height: 50px;
+    background-color: orange;
+    margin: 1rem;
+
+    &:hover {
+      background-color: green;
+    }
+  }
+`;
 
 function App() {
   return (
@@ -21,6 +43,11 @@ class Test extends Component {
     return (
       <div>
         <p>{this.state.content}</p>
+        <Container>
+          <div className="item">item</div>
+          <div className="item">item</div>
+          <div className="item">item</div>
+        </Container>
       </div>
     );
   }
