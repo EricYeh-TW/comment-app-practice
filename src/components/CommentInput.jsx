d56164c9939e13d5202fcb2594c292b306639d49
Eric Yeh <spu8673@gmail.com>
2023-01-12 14:59:22 +0800

    feat: split component & container code

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,67 +1,28 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import StyleInput from '../styles/CommentInput';
-import { useSelector, useDispatch } from 'react-redux';
-import { changeContent, changeUsername } from '../actions/commentAction';
-
-// const mapStateToProps = (state) => {
-//   return {
-//     username: state.commentReducer.username,
-//     content: state.commentReducer.content,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     onChangeUsername: (name) => dispatch(changeUsername(name)),
-//     onChangeContent: (content) => dispatch(changeContent(content)),
-//   };
-// };
+import { useChangeUsername, useChangeContent, useSubmit } from '../containers/CommentInput';
 
 const CommentInput = (props) => {
-  const username = useSelector((state) => state.commentReducer.username);
-  const content = useSelector((state) => state.commentReducer.content);
-  const { onSubmit } = props;
-  const dispatch = useDispatch();
-  useEffect(() => _loadUserName(), []);
-
-  const _saveUserName = () => {
-    localStorage.setItem('username', username);
-  };
-
-  const _loadUserName = () => {
-    const username = localStorage.getItem('username');
-    if (username) dispatch(changeUsername(username));
-  };
-
-  const handleSubmit = () => {
-    const comment = { username, content, time: Date.now() };
-
-    if (comment.username === '' || comment.content === '') return alert('請輸入用戶名稱及內容');
-    if (onSubmit) onSubmit(comment);
-    dispatch(changeContent(''));
-  };
+  const [username, handleUserNameChange] = useChangeUsername();
+  const [content, handleContentChange] = useChangeContent();
+  const [handleSubmit] = useSubmit(props);
 
   return (
     <StyleInput>
       <div className="comment-field">
         <span className="comment-field-name">用戶名稱:</span>
         <div className="comment-field-input">
-          <input
-            onChange={(e) => dispatch(changeUsername(e.target.value))}
-            onBlur={() => _saveUserName()}
-            value={username}
-          />
+          <input onChange={(e) => handleUserNameChange(e.target.value)} value={username} />
         </div>
       </div>
       <div className="comment-field">
         <span className="comment-field-name">評論內容:</span>
         <div className="comment-field-input">
-          <textarea onChange={(e) => dispatch(changeContent(e.target.value))} value={content} />
+          <textarea onChange={(e) => handleContentChange(e.target.value)} value={content} />
         </div>
       </div>
       <div className="comment-field-button">
-        <button type="submit" onClick={() => handleSubmit()}>
+        <button type="submit" onClick={handleSubmit}>
           Submit
         </button>
       </div>
@@ -69,13 +30,4 @@ const CommentInput = (props) => {
   );
 };
 
-CommentInput.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  // onChangeUsername: PropTypes.func,
-  // onChangeContent: PropTypes.func,
-  // username: PropTypes.string,
-  // content: PropTypes.string,
-};
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CommentInput);
 export default CommentInput;
---
+++ src/containers/CommentInput.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { changeContent, changeUsername } from '../actions/commentAction';
+
+let comment = {
+  username: '',
+  content: '',
+  time: 0,
+};
+
+export const useChangeUsername = () => {
+  const username = useSelector((state) => state.commentReducer.username);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const username = localStorage.getItem('username');
+    if (username) handleUserNameChange(username);
+  }, []);
+
+  const handleUserNameChange = (username) => {
+    localStorage.setItem('username', username);
+    dispatch(changeUsername(username));
+  };
+
+  comment = { ...comment, username };
+  return [username, handleUserNameChange];
+};
+
+export const useChangeContent = () => {
+  const content = useSelector((state) => state.commentReducer.content);
+  const dispatch = useDispatch();
+
+  const handleContentChange = (content) => {
+    dispatch(changeContent(content));
+  };
+
+  comment = { ...comment, content };
+  return [content, handleContentChange];
+};
+
+export const useSubmit = (props) => {
+  const { onSubmit } = props;
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    comment = { ...comment, time: Date.now() };
+
+    if (comment.username === '' || comment.content === '') return alert('請輸入用戶名稱及內容');
+    if (onSubmit) onSubmit(comment);
+    dispatch(changeContent(''));
+  };
+
+  return [handleSubmit];
+};
+
+useSubmit.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
